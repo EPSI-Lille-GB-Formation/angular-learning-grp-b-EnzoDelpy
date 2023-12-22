3702895a507de84468b752beef52fc9947c7d0e4
EnzoDelpy <enzodelpy62@gmail.com>
2023-12-22 09:10:06 +0100

    ngfor

--- projects/todolist/src/app/app.component.ts
+++ projects/todolist/src/app/app.component.ts
@@ -1,16 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TODOS } from "./mock-todo"
+import { Todo } from './todo';
+import { Type } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   template: `
-    <h1>Hello World !</h1>
+    <div class='container'>
+      <h1>Liste des choses à faire</h1>
+      <ul>
+        <ng-container *ngFor='let todo of todoList'>
+          <li *ngIf="!todo.isCompleted">{{todo.title}}</li>
+        </ng-container>
+      </ul>
+    </div>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'todolist';
+  todoList = TODOS;
+
+  constructor(){
+    console.table(this.todoList)
+  }
+
+  selectTodo(id : number){
+    return this.todoList[id];
+  }
+
+  getCompletedTask(){
+    return this.todoList.filter(t => !t.isCompleted)
+  }
+
 }
